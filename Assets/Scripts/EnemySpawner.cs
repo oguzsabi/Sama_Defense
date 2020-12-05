cf6952dfa56601390dbf4df8dbd751ee3bc636c7
Oguz Sabitay <oguzsabitay@gmail.com>
2020-12-05 18:36:00 +0300

    Small implementations.
    Changed path colliders to cubes. Added new enemy types. Changed enemy spawner to randomly spawn certain amount of enemies on average.

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,40 +2,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private Transform spawnerLocation;
-    [SerializeField] private GameObject enemy;
+    [SerializeField] private GameObject[] enemyPrefabs;
+    [SerializeField] private float[] enemySpawnPeriods;
+    // [SerializeField] private GameObject enemy;
     [SerializeField] private GameObject[] paths;
-    
-    private const float spawningPeriod = 1.5f;
+
     private float nextSpawnTime;
+    private int enemyPrefabsLength;
     
     // Start is called before the first frame update
     private void Start()
     {
-        
+        enemyPrefabsLength = enemyPrefabs.Length;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (Time.time >  nextSpawnTime)
+        for (var index = 0; index < enemyPrefabsLength; index++)
         {
-            nextSpawnTime = Time.time + spawningPeriod;
-            StartCoroutine(SpawnEnemy());
+            var enemy = enemyPrefabs[index];
+            if (IsSpawnTime(index))
+            {
+                SpawnEnemy(enemy);
+            }
         }
     }
-    
-    private IEnumerator SpawnEnemy()
+
+    private bool IsSpawnTime(int index)
+    {
+        var meanSpawnDelay = enemySpawnPeriods[index];
+        var spawnsPerSecond = 1 / meanSpawnDelay;
+        var threshold = spawnsPerSecond * Time.deltaTime / 5;
+
+        return Random.value < threshold;
+    }
+
+    private void SpawnEnemy(GameObject enemy)
     {
         var newEnemy = Instantiate(enemy, spawnerLocation.position, Quaternion.identity);
         var enemyComponent = newEnemy.GetComponent<Enemy>();
 
         enemyComponent.GetComponent<PathFinder>().SetEnemyPaths(paths);
-        
-        yield return new WaitForSeconds(spawningPeriod);
     }
-    
 }
