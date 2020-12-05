3644376608ac5612aa2da91c3d94d631c2052ed6
DorukMaltepe <55711191+DorukMaltepe@users.noreply.github.com>
2020-12-05 22:58:47 +0300

    Wave Management System implemented

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -11,13 +11,30 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private float[] enemySpawnPeriods;
     // [SerializeField] private GameObject enemy;
     [SerializeField] private GameObject[] paths;
-
+    [SerializeField] private int smallEnemy;
+    [SerializeField] private int normalEnemy;
+    [SerializeField] private int bigEnemy;
+    
+    private int[,] waveArray = new int[1,3];
     private float nextSpawnTime;
     private int enemyPrefabsLength;
-    
+    private int waveCounter = 4; // #of waves in LVL1;
+
+
     // Start is called before the first frame update
     private void Start()
     {
+        
+        waveArray[0,0] = 5; // Small enemy amount in a single wave at LVL1
+        waveArray[0,1] = 5; // Normal enemy amount in single wave at LVL1
+        waveArray[0,2] = 5; // Big enemy amount in single wave at LVL1
+
+        smallEnemy = waveArray[0,0];
+        normalEnemy = waveArray[0,1];
+        bigEnemy = waveArray[0,2];
+      
+        
+        
         enemyPrefabsLength = enemyPrefabs.Length;
     }
 
@@ -27,10 +44,24 @@ public class EnemySpawner : MonoBehaviour
         for (var index = 0; index < enemyPrefabsLength; index++)
         {
             var enemy = enemyPrefabs[index];
-            if (IsSpawnTime(index))
+            if (waveArray[0, 0] == 0 && waveArray[0, 1] == 0 && waveArray[0, 2] == 0)
             {
-                SpawnEnemy(enemy);
+                waveCounter--;
+                waveArray[0, 0] = 5;
+                waveArray[0, 1] = 5;
+                waveArray[0, 2] = 5;
             }
+            else
+            {
+                if (IsSpawnTime(index) && waveArray[0,index] > 0)
+                {
+                    SpawnEnemy(enemy);
+                    waveArray[0,index] -= 1;
+                    print("Enemy Türü: " + index + " Kalan Enemy Sayısı " + waveArray[0,index]);
+                
+                }
+            }
+            
         }
     }
 
