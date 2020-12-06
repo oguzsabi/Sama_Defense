b152ce1fd84feadb104ad471009138924d40ba76
Oguz Sabitay <oguzsabitay@gmail.com>
2020-12-06 18:43:38 +0300

    Wave logic changed
    Now each wave has different number of enemies to spawn and this can be arranged from the editor.

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -11,62 +11,73 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private GameObject[] enemyPrefabs;
     [SerializeField] private float[] enemySpawnPeriods;
     [SerializeField] private GameObject[] paths;
+    [Header("Wave 1 Enemy Count")]
     [Header("Wave Information")]
-    [SerializeField] private int smallEnemy;
-    [SerializeField] private int normalEnemy;
-    [SerializeField] private int bigEnemy;
+    [SerializeField] private int[] firstWaveEnemyCounts;
+    [Header("Wave 2 Enemy Count")]
+    [SerializeField] private int[] secondWaveEnemyCounts;
+    [Header("Wave 3 Enemy Count")]
+    [SerializeField] private int[] thirdWaveEnemyCounts;
+    [Header("Wave 4 Enemy Count")]
+    [SerializeField] private int[] fourthWaveEnemyCounts;
     
-    private readonly int[,] waveArray = new int[4,3];
+    
+    private int[,] waveArray;
+    private int[][] waveEnemyCounts;
     private float nextSpawnTime;
     private int enemyPrefabsLength;
-    private int waveCount = 4; // #of waves in LVL1;
-    private int waveIndex;
-    
+    private int waveCount; // #of waves in LVL1;
+    private int currentWaveIndex;
+
     // Start is called before the first frame update
     private void Start()
     {
-        waveIndex = 0;
-        waveArray[waveIndex, 0] = smallEnemy; // Small enemy amount in a single wave at LVL1
-        waveArray[waveIndex, 1] = normalEnemy; // Normal enemy amount in single wave at LVL1
-        waveArray[waveIndex, 2] = bigEnemy; // Big enemy amount in single wave at LVL1
-
         enemyPrefabsLength = enemyPrefabs.Length;
-        print("currently in wave " + waveIndex);
+        waveCount = 4;
+        currentWaveIndex = 0;
+        waveArray = new int[waveCount, enemyPrefabsLength];
+        waveEnemyCounts = new int[waveCount][];
+        waveEnemyCounts[0] = firstWaveEnemyCounts;
+        waveEnemyCounts[1] = secondWaveEnemyCounts;
+        waveEnemyCounts[2] = thirdWaveEnemyCounts;
+        waveEnemyCounts[3] = fourthWaveEnemyCounts;
+        
+        ArrangeWaveEnemyCounts();
+
+        // waveArray[waveIndex, 0] = smallEnemy; // Small enemy amount in a single wave at LVL1
+        // waveArray[waveIndex, 1] = normalEnemy; // Normal enemy amount in single wave at LVL1
+        // waveArray[waveIndex, 2] = bigEnemy; // Big enemy amount in single wave at LVL1
+        
+        print("currently in wave " + currentWaveIndex);
     }
 
     // Update is called once per frame
     private void Update()
     {
-        for (var index = 0; index < enemyPrefabsLength; index++)
+        for (var enemyIndex = 0; enemyIndex < enemyPrefabsLength; enemyIndex++)
         {
-            var enemy = enemyPrefabs[index];
-            if (waveIndex < waveCount - 1 && waveArray[waveIndex, 0] == 0 && waveArray[waveIndex, 1] == 0 && waveArray[waveIndex, 2] == 0)
-            {
-                waveIndex++;
-                waveArray[waveIndex, 0] = smallEnemy;
-                waveArray[waveIndex, 1] = normalEnemy;
-                waveArray[waveIndex, 2] = bigEnemy;
-                print("currently in wave " + waveIndex);
-            }
-            else
-            {
-                if (IsSpawnTime(index) && waveArray[waveIndex, index] > 0)
-                {
-                    SpawnEnemy(enemy);
-                    waveArray[waveIndex, index] -= 1;
-                    // print("Enemy Türü: " + index + " Kalan Enemy Sayısı " + waveArray[0, index]);
-                }
-            }
+            var enemy = enemyPrefabs[enemyIndex];
+
+            if (!IsSpawnTime(enemyIndex) || IsReadyForNextWave() || !HasSpawnChance(enemyIndex)) continue;
+            
+            SpawnEnemy(enemy);
+            waveArray[currentWaveIndex, enemyIndex] -= 1;
+            // print("Enemy Türü: " + index + " Kalan Enemy Sayısı " + waveArray[0, index]);
         }
     }
 
-    private bool IsSpawnTime(int index)
+    private bool IsSpawnTime(int enemyIndex)
     {
-        var meanSpawnDelay = enemySpawnPeriods[index];
+        var meanSpawnDelay = enemySpawnPeriods[enemyIndex];
         var spawnsPerSecond = 1 / meanSpawnDelay;
-        var threshold = spawnsPerSecond * Time.deltaTime / 5;
+        var threshold = spawnsPerSecond * Time.deltaTime;
 
         return Random.value < threshold;
+    }
+
+    private bool HasSpawnChance(int enemyIndex)
+    {
+        return waveArray[currentWaveIndex, enemyIndex] > 0;
     }
 
     private void SpawnEnemy(GameObject enemy)
@@ -75,5 +86,46 @@ public class EnemySpawner : MonoBehaviour
         var enemyComponent = newEnemy.GetComponent<Enemy>();
 
         enemyComponent.GetComponent<PathFinder>().SetEnemyPaths(paths);
+    }
+
+    private void ArrangeWaveEnemyCounts()
+    {
+        for (var waveNumber = 0; waveNumber < waveCount; waveNumber++)
+        {
+            for (var enemyIndex = 0; enemyIndex < enemyPrefabsLength; enemyIndex++)
+            {
+                waveArray[waveNumber, enemyIndex] = waveEnemyCounts[waveNumber][enemyIndex];
+            }
+        }
+    }
+
+    private bool IsReadyForNextWave()
+    {
+        if (AreAllWavesFinished() || !AreAllEnemiesSpawnedInCurrentWave())
+        {
+            return false;
+        }
+        
+        currentWaveIndex++;
+        print("currently in wave " + currentWaveIndex);
+        return true;
+    }
+
+    private bool AreAllEnemiesSpawnedInCurrentWave()
+    {
+        for (var enemyIndex = 0; enemyIndex < enemyPrefabsLength; enemyIndex++)
+        {
+            if (waveArray[currentWaveIndex, enemyIndex] > 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private bool AreAllWavesFinished()
+    {
+        return currentWaveIndex >= waveCount - 1;
     }
 }
